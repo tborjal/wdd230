7e7598751bb7653474740e54b02ba04714bd75e7
tborjal <bor22014@byui.edu>
2023-10-08 12:41:26 +0800

    added date for javascript

--- chamber/scripts/getdate1.js
+++
@@ -1,9 +0,0 @@
-const currentYear = new Date().getFullYear();
-document.getElementById("current-year").innerHTML = currentYear;
-
-  var lastModDate = new Date(document.lastModified);
-
-  var options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
-  var formattedDate = "Last Modified: " + lastModDate.toLocaleString('en-US', options);
-
-  document.getElementById("last-modified").innerHTML = formattedDate;
