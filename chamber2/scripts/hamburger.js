16d382138a7e840911a5199513309af142bd7cb8
tborjal <bor22014@byui.edu>
2023-10-29 01:57:33 +0800

    changed file names

--- chamber2/scripts/hamburger.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector("#menu");
-const menuList = document.querySelector(".nav_links");
-
-menu.addEventListener("click", () => {
-    menu.classList.toggle("show");
-    menuList.classList.toggle("show");
-});
