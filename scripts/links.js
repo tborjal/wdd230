1fc18d87fece961cf4e48ab2fa6cecb24edbdd17
tborjal <bor22014@byui.edu>
2023-11-07 23:03:59 +0800

    adjusted parameters

--- scripts/links.js
+++ scripts/links.js
@@ -4,7 +4,7 @@ const linksURL = 'https://tborjal.github.io/wdd230/data/links.json';
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data.links);
+    console.log(data);
   }
   
   getLinks();
