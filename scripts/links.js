3d2b18c18ef078723dde4de032dc24dcf2d1da6a
tborjal <bor22014@byui.edu>
2023-11-07 23:35:30 +0800

    adjusted js data

--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,27 @@
 const baseURL = 'https://tborjal.github.io/wdd230/';
 const linksURL = 'https://tborjal.github.io/wdd230/data/links.json';
+const container = document.querySelector('.container3');
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    // console.log(data); 
+    displayLinks(data.lessons);
   }
-  
   getLinks();
+  
+  const displayLinks =  (links) => {
+    links.forEach ((link) => {
 
+    let title = document.createElement('li');
+    let url = document.createElement('a'); 
+
+    title.textContent = `${link.title}`;
+    url.textContent = `${link.title}`;
+    url.href = `${link.url}`;
+    
+    container.appendChild(title);
+    container.appendChild(url);
+
+    });
+  }
