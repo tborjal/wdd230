5862b7b5a6813f83cdf437c54c1b6fd216c36d5b
tborjal <bor22014@byui.edu>
2023-11-07 23:37:38 +0800

    adjusted parameter

--- scripts/links.js
+++ scripts/links.js
@@ -6,7 +6,7 @@ async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     // console.log(data); 
-    displayLinks(data.lessons);
+    displayLinks(data.links);
   }
   getLinks();
   
