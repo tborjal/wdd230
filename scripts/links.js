c13376ce209af2f1971b8fee294929cb012d259b
tborjal <bor22014@byui.edu>
2023-11-08 23:08:02 +0800

    adjusted json file

--- scripts/links.js
+++ scripts/links.js
@@ -3,6 +3,39 @@ const linksURL = 'https://tborjal.github.io/wdd230/data/links.json';
 const container = document.querySelector('.container3');
 
 async function getLinks() {
+  try {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.lessons);
+  } catch (error) {
+    console.error('Error fetching links:', error);
+  }
+}
+
+function displayLinks(lessons) {
+  if (Array.isArray(lessons)) {
+    lessons.forEach((lesson) => {
+      let title = document.createElement('li');
+      let url = document.createElement('a');
+      title.textContent = lesson.title || 'Title not available';
+      url.textContent = lesson.title || 'Title not available';
+      url.href = `${linksURL}${lesson.url}` || '#';
+      container.appendChild(title);
+      container.appendChild(url);
+    });
+  } else {
+    console.log('Invalid JSON data');
+  }
+}
+
+getLinks();
+
+
+/* const baseURL = 'https://tborjal.github.io/wdd230/';
+const linksURL = 'https://tborjal.github.io/wdd230/data/links.json';
+const container = document.querySelector('.container3');
+
+async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     // console.log(data); 
@@ -25,7 +58,7 @@ async function getLinks() {
     
     });
   }
-
+*/
 
 
 
