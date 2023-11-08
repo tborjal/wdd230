41f408cae0859049db43babf90fbd2404e78f2f6
tborjal <bor22014@byui.edu>
2023-11-08 22:01:27 +0800

    adjusted errors

--- scripts/links.js
+++ scripts/links.js
@@ -6,22 +6,49 @@ async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     // console.log(data); 
-    displayLinks(data.links);
+    displayLinks(data.lessons);
   }
   getLinks();
   
-  const displayLinks =  (links) => {
-    links.forEach ((link) => {
+  const displayLinks =  (lessons) => {
+    lessons.forEach ((lesson) => {
 
     let title = document.createElement('li');
     let url = document.createElement('a'); 
 
-    title.textContent = `${link.title}`;
-    url.textContent = `${link.title}`;
-    url.href = `${link.url}`;
-    
+    title.textContent = `${lesson.title}`;
+    url.textContent = `${lesson.title}`;
+    url.href = `${baseURL}${lesson.url}`;
+
     container.appendChild(title);
     container.appendChild(url);
-
+    
     });
   }
+
+
+
+
+ /*  const baseURL = 'https://tborjal.github.io/wdd230/';
+const linksURL = 'https://tborjal.github.io/wdd230/data/links.json';
+const container = document.querySelector('.container3');
+
+async function getLinks() {
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  displayLinks(data.lessons);
+}
+
+function displayLinks(lessons) {
+  lessons.forEach((lesson) => {
+    let title = document.createElement('li');
+    let url = document.createElement('a');
+    title.textContent = `${lesson.title}`;
+    url.textContent = `${lesson.title}`;
+    url.href = `${baseURL}${lesson.url}`;
+    container.appendChild(title);
+    container.appendChild(url);
+  });
+}
+
+getLinks(); */
