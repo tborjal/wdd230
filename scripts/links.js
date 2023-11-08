b124d80ea189fd1a0cd080ae6487c4a38c69f29a
tborjal <bor22014@byui.edu>
2023-11-08 23:29:18 +0800

    adjusted JSON errors and data

--- scripts/links.js
+++ scripts/links.js
@@ -1,36 +1,3 @@
-const baseURL = 'https://tborjal.github.io/wdd230/';
-const linksURL = 'https://tborjal.github.io/wdd230/data/links.json';
-const container = document.querySelector('.container3');
-
-async function getLinks() {
-  try {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    displayLinks(data.lessons);
-  } catch (error) {
-    console.error('Error fetching links:', error);
-  }
-}
-
-function displayLinks(lessons) {
-  if (Array.isArray(lessons)) {
-    lessons.forEach((lesson) => {
-      let title = document.createElement('li');
-      let url = document.createElement('a');
-      title.textContent = lesson.title || 'Title not available';
-      url.textContent = lesson.title || 'Title not available';
-      url.href = `${linksURL}${lesson.url}` || '#';
-      container.appendChild(title);
-      container.appendChild(url);
-    });
-  } else {
-    console.log('Invalid JSON data');
-  }
-}
-
-getLinks();
-
-
 /* const baseURL = 'https://tborjal.github.io/wdd230/';
 const linksURL = 'https://tborjal.github.io/wdd230/data/links.json';
 const container = document.querySelector('.container3');
@@ -57,8 +24,43 @@ async function getLinks() {
     container.appendChild(url);
     
     });
+  } */
+  const baseURL = "https://tborjal.github.io/wdd230/"; // Replace with your actual GitHub Pages URL
+  const linksURL = `${baseURL}data/links.json`;
+
+  async function getLinks() {
+      const response = await fetch(linksURL);
+      const data = await response.json();
+      displayLinks(data);
   }
-*/
+
+  function displayLinks(weeks) {
+      const linksContainer = document.querySelector('.container3');
+
+      for (const week of weeks.lessons) {
+          const weekTitle = `${week.lesson}`;
+          const linksList = document.createElement('ol');
+
+          for (const link of week.links) {
+              const listItem = document.createElement('li');
+              const anchor = document.createElement('a');
+              anchor.href = link.url;
+              anchor.textContent = link.title;
+              listItem.appendChild(anchor);
+              linksList.appendChild(listItem);
+          }
+
+          const weekContainer = document.createElement('div');
+          weekContainer.className = 'week-container';
+          weekContainer.innerHTML = `<p>${weekTitle}</p>`;
+          weekContainer.appendChild(linksList);
+
+          linksContainer.appendChild(weekContainer);
+      }
+  }
+
+  getLinks();
+
 
 
 
