bc9bd038aae74ac95bfce239c94df792d3d64040
tborjal <bor22014@byui.edu>
2023-11-11 22:55:56 +0800

    sync data from JSON

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,31 +1,30 @@
-const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const baseURL = "https://tborjal.github.io/wdd230/"; // Replace with your actual GitHub Pages URL
+const dataURL = `${baseURL}chamber/data/members.json`;
 const cards = document.querySelector('.cards');
 
-async function getProphetData() {
-    const response = await fetch(url);
+async function getCompanyData() {
+    const response = await fetch(dataURL);
     const data = await response.json();
-    // console.table(data.prophets); // temporary testing of data retreival
-    displayProphets(data.prophets);
+    displayCompanies(data.companies);
   }
   
-  getProphetData();
+  getCompanyData();
 
-  const displayProphets =(prophets) => {
-    prophets.forEach ((prophet) => {
+  const displayCompanies =(companies) => {
+    companies.forEach ((company) => {
 
      // Create elements to add to the div.cards element
      let card = document.createElement('section');
-     let fullName = document.createElement('h2'); // fill in the blank
+     let companyName = document.createElement('h3'); // fill in the blank
      let portrait = document.createElement('img');
  
      // Build the h2 content out to show the prophet's full name
-     fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
+     companyName.textContent = `${company.name}`; // fill in the blank
      // Build the image portrait by setting all the relevant attributes
-     portrait.setAttribute('src', prophet.imageurl);
-     portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
+     portrait.setAttribute('src', company.img);
      portrait.setAttribute('loading', 'lazy');
-     portrait.setAttribute('width', '340');
-     portrait.setAttribute('height', '440');
+     portrait.setAttribute('width', '300');
+     portrait.setAttribute('height', '350');
  
      // Append the section(card) with the created elements
      card.appendChild(fullName); //fill in the blank
