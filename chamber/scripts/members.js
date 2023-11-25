b9c5d0b7cc5df36c30a5dac6413c2f3e30b6b05f
tborjal <bor22014@byui.edu>
2023-11-25 14:05:14 +0800

    made final changes the website

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,4 +1,4 @@
-const baseURL = "https://tborjal.github.io/wdd230/"; // Replace with your actual GitHub Pages URL
+/* const baseURL = "https://tborjal.github.io/wdd230/"; // Replace with your actual GitHub Pages URL
 const linksURL = `${baseURL}chamber/data/members.json`;
 
 // Function to load member data from JSON asynchronously
@@ -72,3 +72,164 @@ async function loadMemberData() {
   
   // Initially display members in grid view
   toggleView("grid");
+  
+
+  const baseURL = "https://tborjal.github.io/wdd230/"; // Replace with your actual GitHub Pages URL
+const linksURL = `${baseURL}chamber/data/members.json`;
+
+// Function to load member data from JSON asynchronously
+async function loadMemberData() {
+    try {
+      const response = await fetch(linksURL);
+      const data = await response.json();
+      return data.members;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+ }
+  
+ // Function to display members in grid view
+ function displayGridMembers(members) {
+    const directoryContent = document.querySelector(".directory-content");
+    directoryContent.innerHTML = "";
+  
+    members.forEach((member) => {
+      const memberCard = document.createElement("div");
+      memberCard.className = "member-card";
+  
+      // Create member card content here
+      const cardContent = `
+        <h2>${member.name}</h2>
+        <img src="${member.img}" alt="${member.name} Image" class="member-image">
+        <p>Address: ${member.address}</p>
+        <p>Phone: ${member.phone}</p>
+        <p>Website: <a href="${member.url}" target="_blank">${member.url}</a></p>
+        <p>Membership Level: ${member.level}</p>
+        <p>Established: ${member.year}</p>
+      `;
+      memberCard.innerHTML = cardContent;
+      directoryContent.appendChild(memberCard);
+    });
+ }
+  
+ // Function to display members in list view
+ function displayListMembers(members) {
+    const directoryContent = document.querySelector(".directory-content");
+    directoryContent.innerHTML = "<ul>";
+  
+    members.forEach((member) => {
+      const listItem = document.createElement("li");
+      listItem.className = "list-item";
+      listItem.innerHTML = `
+        <h2>${member.name}</h2>
+        <p>Address: ${member.address}</p>
+        <p>Phone: ${member.phone}</p>
+        <p>Website: <a href="${member.url}" target="_blank">${member.url}</a></p>
+        <p>Membership Level: ${member.level}</p>
+        <p>Established: ${member.year}</p>
+      `;
+      directoryContent.appendChild(listItem);
+    });
+  
+    directoryContent.innerHTML += "</ul>";
+ }
+  
+ // Function to toggle between grid and list view
+ async function toggleView(view) {
+    const membersData = await loadMemberData();
+  
+    if (view === "grid") {
+      displayGridMembers(membersData);
+    } else if (view === "list") {
+      displayListMembers(membersData);
+    }
+ }
+  
+ // Initially display members in grid view
+ toggleView("grid");
+ */
+
+ const baseURL = "https://tborjal.github.io/wdd230/"; // Replace with your actual GitHub Pages URL
+const linksURL = `${baseURL}chamber/data/members.json`;
+
+// Function to load member data from JSON asynchronously
+async function loadMemberData() {
+    try {
+      const response = await fetch(linksURL);
+      const data = await response.json();
+      return data.members;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+ }
+  
+ // Function to display members in grid view
+ function displayGridMembers(members) {
+    const directoryContent = document.querySelector(".directory-content");
+    directoryContent.innerHTML = "";
+  
+    members.forEach((member) => {
+      const memberCard = document.createElement("div");
+      memberCard.className = "member-card";
+  
+      // Create member card content here
+      const cardContent = `
+        <h2>${member.name}</h2>
+        <img src="${member.img}" alt="${member.name} Image" class="member-image">
+        <p>Address: ${member.address}</p>
+        <p>Phone: ${member.phone}</p>
+        <p>Website: <a href="${member.url}" target="_blank">${member.url}</a></p>
+        <p>Membership Level: ${member.level}</p>
+        <p>Established: ${member.year}</p>
+      `;
+      memberCard.innerHTML = cardContent;
+      directoryContent.appendChild(memberCard);
+    });
+ }
+  
+ // Function to display members in list view
+ function displayListMembers(members) {
+    const directoryContent = document.querySelector(".directory-content");
+    directoryContent.innerHTML = "<ul>";
+  
+    members.forEach((member) => {
+      const listItem = document.createElement("li");
+      listItem.className = "list-item";
+      listItem.innerHTML = `
+        <h2>${member.name}</h2>
+        <p>Address: ${member.address}</p>
+        <p>Phone: ${member.phone}</p>
+        <p>Website: <a href="${member.url}" target="_blank">${member.url}</a></p>
+        <p>Membership Level: ${member.level}</p>
+        <p>Established: ${member.year}</p>
+      `;
+      directoryContent.appendChild(listItem);
+    });
+  
+    directoryContent.innerHTML += "</ul>";
+ }
+  
+ // Function to toggle between grid and list view
+ async function toggleView(view) {
+    const membersData = await loadMemberData();
+  
+    if (view === "grid") {
+      displayGridMembers(membersData);
+    } else if (view === "list") {
+      displayListMembers(membersData);
+    }
+ }
+  
+ // Listen for view changes and update accordingly
+ const viewToggleButtons = document.querySelectorAll("[data-view]");
+ viewToggleButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const view = button.getAttribute("data-view");
+      toggleView(view);
+    });
+ });
+  
+ // Initially display members in grid view
+ toggleView("grid");
