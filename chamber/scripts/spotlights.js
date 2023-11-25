cbcb324dc54a4a2120a8f73bf35c18b766d9303a
tborjal <bor22014@byui.edu>
2023-11-25 13:01:01 +0800

    adjusted media queries

--- chamber/scripts/spotlights.js
+++ chamber/scripts/spotlights.js
@@ -149,7 +149,7 @@ function shuffleArray(array) {
 
     return array;
 }
-*/
+
 const baseURL = "https://tborjal.github.io/wdd230/"; // Replace with your actual GitHub Pages URL
 const dataURL = `${baseURL}chamber/data/members.json`
 
@@ -181,6 +181,62 @@ fetch(dataURL)
         `;
 
         document.getElementById('spotlights').appendChild(memberDiv);
+    });
+});
+
+function shuffleArray(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there are elements to shuffle...
+    while (currentIndex !== 0) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+*/
+
+const baseURL = "https://tborjal.github.io/wdd230/"; // Replace with your actual GitHub Pages URL
+const dataURL = `${baseURL}chamber/data/members.json`
+
+fetch(dataURL)
+.then(response => response.json())
+.then(data => {
+    // Filter out members with a gold or silver level
+    const goldMembers = data.members.filter(member => member.level === 'Gold');
+    const silverMembers = data.members.filter(member => member.level === 'Silver');
+
+    // Randomly select 2 to 3 members from each array.
+    const shuffledGoldMembers = shuffleArray(goldMembers).slice(0, Math.floor(Math.random() * (2 - 1 + 1)) + 1);
+    const shuffledSilverMembers = shuffleArray(silverMembers).slice(0, Math.floor(Math.random() * (2 - 1 + 1)) + 1);
+
+    // Combine the selected members arrays.
+    const shuffledMembers = [...shuffledGoldMembers, ...shuffledSilverMembers];
+
+    // Limit the displayed members to 3
+    const limitedMembers = shuffledMembers.slice(0, 3);
+
+    // Display each member's data.
+    limitedMembers.forEach(members => {
+        const memberDiv = document.createElement('div');
+        memberDiv.innerHTML = `
+            <h2>${members.name}</h2>
+            <p>Level: ${members.level}</p>
+            <p>Year: ${members.year}</p>
+            <img src="${members.img}" alt="${members.name}" width="200" height="150">
+        `;
+
+        document.querySelectorAll('.spotlights').forEach(spotlight => {
+            spotlight.appendChild(memberDiv.cloneNode(true));
+        });
     });
 });
 
