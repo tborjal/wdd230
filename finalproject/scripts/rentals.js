067a765de0faecc87ef6c2a22b8b4bc03c26ad52
tborjal <bor22014@byui.edu>
2023-12-11 01:36:44 +0800

    applied changes

--- finalproject/scripts/rentals.js
+++ finalproject/scripts/rentals.js
@@ -1,22 +1,23 @@
 const baseURL = "https://tborjal.github.io/wdd230/"; // Replace with your actual GitHub Pages URL
 const linksURL = `${baseURL}finalproject/data/pricing.json`;
 
-fetch(linksURL)
-    .then(response => response.json())
-    .then(data => {
-        const tableBody = document.getElementById('tableBody');
+fetch(linkURL)
+.then(response => response.json())
+.then(data => {
+  const tableBody = document.getElementById('tableBody');
 
-        data.forEach(item => {
-            const row = `<tr>
-                                <td>${item["Rental Type"]}</td>
-                                <td>${item["Max. Persons"]}</td>
-                                <td>${item["Half Day (3 hrs)"]}</td>
-                                <td>${item["Full Day"]}</td>
-                                <td>${item["Half Day (3 hrs)"]}</td>
-                                <td>${item["Full Day"]}</td>
-                           </tr>`;
-            tableBody.innerHTML += row;
-        });
-    })
-    .catch(error => console.error('Error:', error));
-    
+  data.rentalType.forEach(item => {
+    const row = `
+      <tr>
+        <td>${item["type"]}</td>
+        <td>${item["maxPersons"]}</td>
+        <td>${item["halfDayReservationPrice"]}</td>
+        <td>${item["fullDayReservationPrice"]}</td>
+        <td>${item["halfDayWalkInPrice"]}</td>
+        <td>${item["fullDayWalkInPrice"]}</td>
+      </tr>
+    `;
+
+    tableBody.innerHTML += row;
+  });
+});
