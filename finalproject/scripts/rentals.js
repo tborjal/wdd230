dc5691c81f0c331dd12f218f94f9743d64fbf1f0
tborjal <bor22014@byui.edu>
2023-12-11 00:47:35 +0800

    sync JSON file

--- finalproject/scripts/rentals.js
+++ finalproject/scripts/rentals.js
@@ -1,14 +1,16 @@
-fetch('/data/pricing.json')
-            .then(response => response.json())
-            .then(data => {
-                const table = document.getElementById('rentalTable');
-                data.forEach(item => {
-                    const row = table.insertRow();
-                    Object.values(item).forEach(val => {
-                        const cell = row.insertCell();
-                        cell.textContent = val;
-                    });
-                });
-            });
+fetch('..data/pricing.json')
+    .then(response => response.json())
+    .then(data => {
+        const tableBody = document.getElementById('tableBody');
 
+        data.forEach(item => {
+            const row = `<tr>
+                                <td>${item["Rental Type"]}</td>
+                                <td>${item["Half Day (3 hrs)"]}</td>
+                                <td>${item["Full Day"]}</td>
+                           </tr>`;
+            tableBody.innerHTML += row;
+        });
+    })
+    .catch(error => console.error('Error:', error));
     
