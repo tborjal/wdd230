e23d193c5110f8da8a7d62e7b3402e1ddffb9a7d
tborjal <bor22014@byui.edu>
2023-12-10 23:19:09 +0800

    revert changes

--- finalproject/scripts/rentals.js
+++ finalproject/scripts/rentals.js
@@ -1,4 +1,4 @@
-/* fetch('data/pricing.json')
+fetch('data/pricing.json')
             .then(response => response.json())
             .then(data => {
                 const table = document.getElementById('rentalTable');
@@ -10,32 +10,5 @@
                     });
                 });
             });
-*/
-            fetch('data/pricing.json')
-            .then(response => response.json())
-            .then(data => {
-                let vehicles = data.web.vehicles;
-                let output = '';
-
-                for (let vehicle in vehicles) {
-                    output += `
-                        <h2>${vehicles[vehicle].title}</h2>
-                        <p>Maximum Passengers: ${vehicles[vehicle].max_persons}</p>
-                        <p>Rental Types:</p>
-                        <ul>
-                    `;
-
-                    for (let rental_type in vehicles[vehicle].rental_types) {
-                        output += `
-                            <li>${rental_type} - $${vehicles[vehicle].rental_types[rental_type].price}</li>
-                        `;
-                    }
-
-                    output += '</ul>';
-                }
-
-                document.getElementById('rentalTable').innerHTML = output;
-            })
-            .catch(error => console.error('Error:', error));
 
     
